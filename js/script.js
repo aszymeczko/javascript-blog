576492ac65acac510177f74e2e5cbea00ddd502d
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-10-05 23:08:17 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -29,9 +30,20 @@ function titleClickHandler(event){
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute("href");
+
+  console.log("articleSelector: ", articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log("targetArticle: ", targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
