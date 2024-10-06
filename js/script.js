2444e2cb69cfd9845aa1b1aa02cd23da5084af8f
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-10-06 19:34:14 +0200

    Remove list of links

--- js/script.js
+++ js/script.js
@@ -51,3 +51,33 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+    /* get the article id */
+
+    const articles = optArticleSelector
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+    console.log ('function works')
+}
+
+generateTitleLinks();
